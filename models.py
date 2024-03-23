e1086269d5b3121215a4a59fbeca6d0df2c03f55
soroushb <sbahrami41@yahoo.com>
2024-03-23 17:07:32 -0400

    models made

--- models.py
+++ models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# Add code to create Menu model
+class Menu(models.Model):
+   title = models.CharField(max_length=255) 
+   price = models.FloatField(null=False) 
+   inventory = models.IntegerField(max_length=5)
+
+   def __str__(self):
+      return self.name
+   
+class Booking(models.Model):
+   name = models.CharField(max_length = 255)
+   no_of_guests = models.IntegerField(6)
+   bookingDate = models.DateTimeField
